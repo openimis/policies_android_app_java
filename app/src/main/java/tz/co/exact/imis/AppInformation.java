ef3e7e5bdb19f9fb8e6797f2efb6de54f4f16e25
Dragos DOBRE <dobre.dragos@gmail.com>
2018-09-10 11:54:49 +0200

    First commit

---
+++ app/src/main/java/tz/co/exact/imis/AppInformation.java
@@ -0,0 +1,15 @@
+package tz.co.exact.imis;
+
+/**
+ * Created by Hiren on 3/16/2018.
+ */
+
+ public class AppInformation {
+     public static class DomainInfo {
+         private static String _Domain = "http://132.148.151.32/";
+
+         public static String getDomain(){
+             return _Domain;
+         }
+     }
+}
