6d84797766708a75b22f9869431699406f533c72
Dragos Dobre <dragos.dobre@swisstph.ch>
2018-10-03 12:17:38 +0200

    Update openImis demo URL

--- app/src/main/java/tz/co/exact/imis/AppInformation.java
+++ app/src/main/java/tz/co/exact/imis/AppInformation.java
@@ -3,8 +3,8 @@ package tz.co.exact.imis;
  public class AppInformation {
      public static class DomainInfo {
 
-         // IP address of openIMIS demo server => please update with your own Web Services base URL/IP
-         private static String _Domain = "http://132.148.151.32/";
+         // URL address of openIMIS demo server => please update with your own Web Services base URL/IP
+         private static String _Domain = "http://demo.openimis.org/";
 
          public static String getDomain(){
              return _Domain;
