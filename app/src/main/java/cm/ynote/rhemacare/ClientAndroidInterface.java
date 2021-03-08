0afe2c9fdb7815e09a82270374ac15b44052a9c6
maxime ngoe <maxime.ngoe@gmail.com>
2021-03-08 11:39:14 +0100

    Form Presentation

--- app/src/main/java/cm/ynote/rhemacare/ClientAndroidInterface.java
+++ app/src/main/java/cm/ynote/rhemacare/ClientAndroidInterface.java
@@ -3,22 +3,22 @@
 //The program users must agree to the following terms:
 //
 //Copyright notices
-//This program is free software: you can redistribute it and/or modify it under the terms of the GNU AGPL v3 License as published by the 
+//This program is free software: you can redistribute it and/or modify it under the terms of the GNU AGPL v3 License as published by the
 //Free Software Foundation, version 3 of the License.
-//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU AGPL v3 License for more details www.gnu.org.
 //
 //Disclaimer of Warranty
-//There is no warranty for the program, to the extent permitted by applicable law; except when otherwise stated in writing the copyright 
-//holders and/or other parties provide the program "as is" without warranty of any kind, either expressed or implied, including, but not 
-//limited to, the implied warranties of merchantability and fitness for a particular purpose. The entire risk as to the quality and 
+//There is no warranty for the program, to the extent permitted by applicable law; except when otherwise stated in writing the copyright
+//holders and/or other parties provide the program "as is" without warranty of any kind, either expressed or implied, including, but not
+//limited to, the implied warranties of merchantability and fitness for a particular purpose. The entire risk as to the quality and
 //performance of the program is with you. Should the program prove defective, you assume the cost of all necessary servicing, repair or correction.
 //
-//Limitation of Liability 
-//In no event unless required by applicable law or agreed to in writing will any copyright holder, or any other party who modifies and/or 
-//conveys the program as permitted above, be liable to you for damages, including any general, special, incidental or consequential damages 
-//arising out of the use or inability to use the program (including but not limited to loss of data or data being rendered inaccurate or losses 
-//sustained by you or third parties or a failure of the program to operate with any other programs), even if such holder or other party has been 
+//Limitation of Liability
+//In no event unless required by applicable law or agreed to in writing will any copyright holder, or any other party who modifies and/or
+//conveys the program as permitted above, be liable to you for damages, including any general, special, incidental or consequential damages
+//arising out of the use or inability to use the program (including but not limited to loss of data or data being rendered inaccurate or losses
+//sustained by you or third parties or a failure of the program to operate with any other programs), even if such holder or other party has been
 //advised of the possibility of such damages.
 //
 //In case of dispute arising out or in relation to the use of the program, it is subject to the public law of Switzerland. The place of jurisdiction is Berne.
@@ -574,7 +574,7 @@ public class ClientAndroidInterface {
 
         try {
             object.put("Code", "");
-            object.put("Status", mContext.getResources().getString(R.string.SelectNetworkProvider));
+            object.put("Status", mContext.getResources().getString(R.string.SelectPaiementProvider));
             paiementProvider.put(object);
 
             object = new JSONObject();
@@ -604,7 +604,7 @@ public class ClientAndroidInterface {
 
         try {
             object.put("Code", "");
-            object.put("Status", mContext.getResources().getString(R.string.SelectNetworkProvider));
+            object.put("Status", mContext.getResources().getString(R.string.SelectHHStatus));
             statusProvider.put(object);
 
             object = new JSONObject();
@@ -625,6 +625,118 @@ public class ClientAndroidInterface {
             e.printStackTrace();
         }
         return statusProvider.toString();
+    }
+
+
+    @JavascriptInterface
+    public String getLengthStayProvider() {
+        JSONArray lengthStayProvider = new JSONArray();
+        JSONObject object = new JSONObject();
+
+        try {
+            object.put("Code", "");
+            object.put("Status", mContext.getResources().getString(R.string.selectLengthStay));
+            lengthStayProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "l1");
+            object.put("Status", mContext.getResources().getString(R.string.Lessthan1year));
+            lengthStayProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "l3");
+            object.put("Status", mContext.getResources().getString(R.string.Lessthan3years));
+            lengthStayProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "m3");
+            object.put("Status", mContext.getResources().getString(R.string.Morethan3years));
+            lengthStayProvider.put(object);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return lengthStayProvider.toString();
+    }
+
+
+
+    @JavascriptInterface
+    public String gethHStatusProvider() {
+        JSONArray hhStatusProvider = new JSONArray();
+        JSONObject object = new JSONObject();
+
+        try {
+            object.put("Code", "");
+            object.put("Status", mContext.getResources().getString(R.string.SelectHealthStatus));
+            hhStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "H");
+            object.put("Status", mContext.getResources().getString(R.string.Healthy));
+            hhStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "FS");
+            object.put("Status", mContext.getResources().getString(R.string.Frequentlysick));
+            hhStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "VS");
+            object.put("Status", mContext.getResources().getString(R.string.VerySick));
+            hhStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "TI");
+            object.put("Status", mContext.getResources().getString(R.string.TerminallyIll));
+            hhStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "O");
+            object.put("Status", mContext.getResources().getString(R.string.Disability));
+            hhStatusProvider.put(object);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return hhStatusProvider.toString();
+    }
+
+    @JavascriptInterface
+    public String getNutritionalStatusProvider() {
+        JSONArray nutritionalStatusProvider = new JSONArray();
+
+        JSONObject object = new JSONObject();
+
+
+        try {
+            object.put("Code", "");
+            object.put("Status", mContext.getResources().getString(R.string.SelectNutritional));
+            nutritionalStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "ON");
+            object.put("Status", mContext.getResources().getString(R.string.once));
+            nutritionalStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "TW");
+            object.put("Status", mContext.getResources().getString(R.string.twice));
+            nutritionalStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "THR");
+            object.put("Status", mContext.getResources().getString(R.string.threeMore));
+            nutritionalStatusProvider.put(object);
+
+            object = new JSONObject();
+            object.put("Code", "O");
+            object.put("Status", mContext.getResources().getString(R.string.None));
+            nutritionalStatusProvider.put(object);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return nutritionalStatusProvider.toString();
     }
 
     @JavascriptInterface
