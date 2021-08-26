57a9976bd4ed9a7d700bbe4be866702268d8e3d9
malinowskikam <kmalinowski@soldevelo.com>
2021-08-26 16:03:16 +0200

    OTC-368: Added BulkControlNumbersActivity

--- app/src/main/java/org/openimis/imispolicies/PaymentOverview.java
+++ app/src/main/java/org/openimis/imispolicies/PaymentOverview.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class PaymentOverview extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class PaymentOverview extends AppCompatActivity {
     TextView NotEnrolledPolicies;
     TextView OverViewControlNumber;
     TextView CheckCommission;
+    TextView BulkControlNumber;
 
 
     @Override
@@ -19,7 +21,7 @@ public class PaymentOverview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_overview);
         final ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null) {
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle(getResources().getString(R.string.PaymentOverview));
         }
@@ -28,29 +30,29 @@ public class PaymentOverview extends AppCompatActivity {
         NotEnrolledPolicies = findViewById(R.id.NotEnrolledPolicies);
         OverViewControlNumber = findViewById(R.id.OverViewControlNumber);
         CheckCommission = findViewById(R.id.CheckCommission);
+        BulkControlNumber = findViewById(R.id.BulkControlNumber);
 
-
+        BulkControlNumber.setVisibility(AppInformation.MenuInfo.getShowBulkCNMenu() ? View.VISIBLE : View.GONE);
 
         OverViewPolicies.setOnClickListener(view -> openPage(SearchOverViewPolicies.class));
         NotEnrolledPolicies.setOnClickListener(view -> openPage(SearchNotEnrolledPolicies.class));
         OverViewControlNumber.setOnClickListener(view -> openPage(SearchOverViewControlNumber.class));
+        BulkControlNumber.setOnClickListener(view -> openPage(BulkControlNumbersActivity.class));
         CheckCommission.setOnClickListener(view -> openPage(CheckCommission.class));
-
     }
 
-    public void openPage(Class<? extends AppCompatActivity> page)
-    {
+    public void openPage(Class<? extends AppCompatActivity> page) {
         Intent intent = new Intent(this, page);
         startActivity(intent);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
         return true;
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
-
 }
