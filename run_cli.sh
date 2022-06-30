aeb09a709749dedd7c576d86da99aca1e94b9648
vaneck237 <vaneckbilongo@gmail.com>
2022-06-30 11:22:18 +0100

    delete contents

--- run_cli.sh
+++
@@ -1,1 +0,0 @@
-echo '{"api_base_url":"http://192.168.0.100/rest/", "app_name":"IMIS Polices Gaya", "app_dir":"IMIS-POL-GAYA", "application_id":"org.openimis.imispolicies.niger", "cli_java_dir": "src/niger/java", "cli_res_dir": "src/niger/res"}' | gh workflow run manual.yml --ref develop --json
