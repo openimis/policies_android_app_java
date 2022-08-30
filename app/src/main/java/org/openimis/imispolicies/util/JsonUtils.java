281b1986d39ad8703691422670e09556feea9037
malinowskikam <kmalinowski@soldevelo.com>
2022-08-30 17:27:11 +0200

    OTC-683 Fixed no InsureePolicy after Policy

--- app/src/main/java/org/openimis/imispolicies/util/JsonUtils.java
+++ app/src/main/java/org/openimis/imispolicies/util/JsonUtils.java
@@ -30,4 +30,23 @@ public class JsonUtils {
             return true;
         }
     }
+
+    /**
+     * @param object          Json object
+     * @param field           field to be checked
+     * @param defaultValue    default value to be used if requested value does not exists
+     * @param checkNullString Should "null" string be considered empty
+     * @return value of the specified field if exists, default value otherwise
+     */
+    public static String getStringOrDefault(@NonNull JSONObject object, @NonNull String field, String defaultValue, boolean checkNullString) {
+        try {
+            if (!isStringEmpty(object, field, checkNullString)) {
+                return object.getString(field);
+            } else {
+                return defaultValue;
+            }
+        } catch (JSONException e) {
+            return defaultValue;
+        }
+    }
 }
