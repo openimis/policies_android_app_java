af8aa9eb7c152ae5346e8e9e43639c8a002c3cc6
vaneck237 <vaneckbilongo@gmail.com>
2025-03-20 10:03:04 +0100

    get csrfToken

---
+++ app/src/main/java/org/openimis/imispolicies/network/request/GetCsrfTokenGraphQLRequest.java
@@ -0,0 +1,11 @@
+package org.openimis.imispolicies.network.request;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
+
+public class GetCsrfTokenGraphQLRequest extends BaseGraphQLRequest {
+
+    @WorkerThread
+    @NonNull
+    public Get
+}
--- app/src/main/java/org/openimis/imispolicies/usecase/Login.java
+++ app/src/main/java/org/openimis/imispolicies/usecase/Login.java
@@ -11,6 +11,7 @@ import org.openimis.imispolicies.Token;
 import org.openimis.imispolicies.network.dto.LoginDto;
 import org.openimis.imispolicies.network.dto.TokenDto;
 import org.openimis.imispolicies.network.exception.HttpException;
+import org.openimis.imispolicies.network.request.GetCsrfTokenGraphQLRequest;
 import org.openimis.imispolicies.network.request.LoginRequest;
 import org.openimis.imispolicies.repository.LoginRepository;
 
@@ -30,6 +31,7 @@ public class Login {
     @NonNull
     private final ToRestApi toRestApi;
     private final boolean isPaymentEnabled;
+    private final GetCsrfTokenGraphQLRequest csrfRequest;
 
     public Login() {
         this(Global.getGlobal().getLoginRepository(), new LoginRequest(), new ToRestApi(), BuildConfig.IS_PAYMENT_ENABLED);
@@ -56,6 +58,8 @@ public class Login {
         try {
             TokenDto token = request.post(new LoginDto(username.trim(), password));
             repository.saveFhirToken(token.getToken(), new Date(token.getExpiresOn()), officerCode);
+            fetchCsrfToken(token.getToken());
+
             if (isPaymentEnabled) {
                 token = loginToRestApi(username, password);
                 repository.saveRestToken(token.getToken(), new Date(token.getExpiresOn()), officerCode);
@@ -84,5 +88,9 @@ public class Login {
                 /* token = */ ob.getString("access_token"),
                 /* expiresOn = */ Token.getValidity(ob.getString("expires_on")).getTime()
         );
+    }
+
+    private void fetchCsrfToken(@NonNull String token){
+
     }
 }
