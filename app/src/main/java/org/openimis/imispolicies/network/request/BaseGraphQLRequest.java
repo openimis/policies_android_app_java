0f6bd0db7514e49bc54a83873500ef3062c70372
vaneck237 <vaneckbilongo@gmail.com>
2025-04-09 17:45:01 +0100

    change post method for queries to GET method

--- app/src/main/java/org/openimis/imispolicies/network/request/BaseGraphQLRequest.java
+++ app/src/main/java/org/openimis/imispolicies/network/request/BaseGraphQLRequest.java
@@ -31,6 +31,7 @@ public abstract class BaseGraphQLRequest {
 
     private static final ApolloClient apolloClient = ApolloClient.builder()
             .okHttpClient(OkHttpUtils.getDefaultOkHttpClient())
+            .useHttpGetMethodForQueries(true)
             .serverUrl(URI)
             .addCustomTypeAdapter(CustomType.DATE, new DateCustomTypeAdapter())
             .addCustomTypeAdapter(CustomType.DATETIME, new DateTimeCustomTypeAdapter())
