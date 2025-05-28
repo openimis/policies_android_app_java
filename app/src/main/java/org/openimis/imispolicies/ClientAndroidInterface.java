ce618f15d7a90dee0f9d6ff8f926b18b8826ae2a
pauldjeuda <115945610+pauldjeuda@users.noreply.github.com>
2025-05-28 12:54:33 +0100

    Update ClientAndroidInterface.java

--- app/src/main/java/org/openimis/imispolicies/ClientAndroidInterface.java
+++ app/src/main/java/org/openimis/imispolicies/ClientAndroidInterface.java
@@ -4618,7 +4618,7 @@ public class ClientAndroidInterface {
     @SuppressWarnings("unused")
     public int getTotalFamily() {
         @Language("SQL")
-        String FamilyQuery = "SELECT count(1) Families  FROM  tblfamilies WHERE isoffline = 1 OR isoffline = 0"; // WHERE isoffline = 1 OR isoffline = 0
+        String FamilyQuery = "SELECT count(1) Families  FROM  tblfamilies WHERE isoffline !=''"; // WHERE isoffline = 1 OR isoffline = 0
         JSONArray Families = sqlHandler.getResult(FamilyQuery, null);
         JSONObject object = null;
         int TotalFamilies = 0;
