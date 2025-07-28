0cba37b2e0095c58f5e86d6f19ef387fb665bdbf
irs269 <abdouelanzize95@gmail.com>
2025-07-28 09:46:51 +0300

    Fix: Add missing fields to getInsuree() SQL query
    - Added NoDisability, NonDisablingDisease, MutualInsuranceCoverage, HousingType to SELECT query
    - Added sqlHandler.initializeNewColumns() call to ensure database columns exist
    - Fixes issue where these fields were empty during insuree editing
    
    Resolves insuree data retrieval problem for new mandatory fields.

--- app/src/main/java/org/openimis/imispolicies/ClientAndroidInterface.java
+++ app/src/main/java/org/openimis/imispolicies/ClientAndroidInterface.java
@@ -1365,8 +1365,11 @@ public class ClientAndroidInterface {
     @JavascriptInterface
     @SuppressWarnings("unused")
     public String getInsuree(int InsureeId) {
+        // Ensure all new columns exist in tblInsuree before querying
+        sqlHandler.initializeNewColumns();
+        
         @Language("SQL")
-        String Query = "SELECT InsureeId, FamilyId, CHFID, LastName, OtherNames, DOB, Gender, Marital, isHead, IdentificationNumber, Phone, isOffline , PhotoPath, CardIssued, Relationship, Profession, Education, Email, TypeOfId, I.HFID, CurrentAddress,R.LocationId CurRegion, D.LocationId CurDistrict, W.LocationId CurWard,  I.CurVillage, HFR.LocationId FSPRegion, HFD.LocationId FSPDistrict, HF.HFLevel FSPCategory, I.Vulnerability, ProfessionalSituation, IncomeLevel, ResidenceEnvironment, PaymentMethod, OtherHousehold, AccountDetails\n" +
+        String Query = "SELECT InsureeId, FamilyId, CHFID, LastName, OtherNames, DOB, Gender, Marital, isHead, IdentificationNumber, Phone, isOffline , PhotoPath, CardIssued, Relationship, Profession, Education, Email, TypeOfId, I.HFID, CurrentAddress,R.LocationId CurRegion, D.LocationId CurDistrict, W.LocationId CurWard,  I.CurVillage, HFR.LocationId FSPRegion, HFD.LocationId FSPDistrict, HF.HFLevel FSPCategory, I.Vulnerability, ProfessionalSituation, IncomeLevel, ResidenceEnvironment, PaymentMethod, OtherHousehold, AccountDetails, NoDisability, NonDisablingDisease, MutualInsuranceCoverage, HousingType\n" +
                 "FROM tblInsuree I\n" +
                 "LEFT OUTER JOIN tblLocations V ON V.LocationId = I.CurVillage\n" +
                 "LEFT OUTER JOIN tblLocations W ON W.LocationId = V.ParentLocationId\n" +
