f78968a51b0063537be42c09bc49ccd033082a8f
vaneck237 <vaneckbilongo@gmail.com>
2026-04-27 12:19:13 +0100

    adjust PR

--- app/src/main/java/org/openimis/imispolicies/util/AndroidUtils.java
+++ app/src/main/java/org/openimis/imispolicies/util/AndroidUtils.java
@@ -6,7 +6,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Looper;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
